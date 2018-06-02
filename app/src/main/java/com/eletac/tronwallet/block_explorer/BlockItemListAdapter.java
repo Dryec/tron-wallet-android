920d2399e20d0790eb9719f356f1821f2bc7d814
Marius Gill <35896407+Dryec@users.noreply.github.com>
2018-06-02 07:45:59 +0200

    added block elapsed time updater

--- app/src/main/java/com/eletac/tronwallet/block_explorer/BlockItemListAdapter.java
+++ app/src/main/java/com/eletac/tronwallet/block_explorer/BlockItemListAdapter.java
@@ -1,8 +1,8 @@
 package com.eletac.tronwallet.block_explorer;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateUtils;
 import android.view.LayoutInflater;
@@ -22,10 +22,33 @@ public class BlockItemListAdapter extends RecyclerView.Adapter<BlockItemListAdap
 
     private Context mContext;
     private List<Protocol.Block> mBlocks;
+    private Handler mUpdateElapsedTimeHandler;
+    private List<BlockItemViewHolder> mViewHolders;
+    Runnable mUpdateElapsedTimeRunnable = new Runnable() {
+        public void run() {
+            List<BlockItemViewHolder> viewHoldersToRemove = new ArrayList<>();
+            for (BlockItemViewHolder viewHolder : mViewHolders) {
+                if(viewHolder != null)
+                    viewHolder.updateElapsedTime();
+                else
+                    viewHoldersToRemove.add(viewHolder);
+            }
+
+            for (BlockItemViewHolder viewHolder : viewHoldersToRemove) {
+                mViewHolders.remove(viewHolder);
+            }
+
+            mUpdateElapsedTimeHandler.postDelayed(mUpdateElapsedTimeRunnable, 1010);
+        }
+    };
 
     public BlockItemListAdapter(Context context, List<Protocol.Block> blocks) {
         mContext = context;
         mBlocks = blocks;
+        mViewHolders = new ArrayList<>();
+
+        mUpdateElapsedTimeHandler = new Handler();
+        mUpdateElapsedTimeHandler.postDelayed(mUpdateElapsedTimeRunnable,2000);
     }
 
     @NonNull
@@ -33,6 +56,7 @@ public class BlockItemListAdapter extends RecyclerView.Adapter<BlockItemListAdap
     public BlockItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_block_item, parent, false);
         BlockItemViewHolder viewHolder = new BlockItemViewHolder(view);
+        mViewHolders.add(viewHolder);
         return viewHolder;
     }
 
@@ -42,9 +66,15 @@ public class BlockItemListAdapter extends RecyclerView.Adapter<BlockItemListAdap
     }
 
     @Override
+    public void onViewRecycled(@NonNull BlockItemViewHolder holder) {
+        super.onViewRecycled(holder);
+    }
+
+    @Override
     public int getItemCount() {
         return mBlocks != null ? mBlocks.size() : 0;
     }
+
 
     public class BlockItemViewHolder extends RecyclerView.ViewHolder {
         private Context mContext;
@@ -53,6 +83,8 @@ public class BlockItemListAdapter extends RecyclerView.Adapter<BlockItemListAdap
         private TextView mBlockElapsedTime_TextView;
         private TextView mBlockTransactionsAmount_TextView;
         private TextView mBlockProducerAddress_TextView;
+
+        private Protocol.Block mBlock;
 
         public BlockItemViewHolder(View itemView) {
             super(itemView);
@@ -65,14 +97,21 @@ public class BlockItemListAdapter extends RecyclerView.Adapter<BlockItemListAdap
         }
 
         public void bind(Protocol.Block block) {
-            String blockNumberStr = "#"+String.valueOf(block.getBlockHeader().getRawData().getNumber());
-            CharSequence timeString = DateUtils.getRelativeTimeSpanString(block.getBlockHeader().getRawData().getTimestamp(), System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS);
+            mBlock = block;
 
+            String blockNumberStr = "#"+String.valueOf(block.getBlockHeader().getRawData().getNumber());
             mBlockNumber_TextView.setText(blockNumberStr);
-            mBlockElapsedTime_TextView.setText(timeString);
+            updateElapsedTime();
             mBlockTransactionsAmount_TextView.setText(String.valueOf(block.getTransactionsCount()));
             mBlockProducerAddress_TextView.setText(WalletClient.encode58Check(block.getBlockHeader().getRawData().getWitnessAddress().toByteArray()));
         }
 
+        public void updateElapsedTime() {
+            if(mBlock != null) {
+                CharSequence timeString = DateUtils.getRelativeTimeSpanString(mBlock.getBlockHeader().getRawData().getTimestamp(), System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS);
+                mBlockElapsedTime_TextView.setText(timeString);
+                mBlockElapsedTime_TextView.setText(timeString);
+            }
+        }
     }
 }
