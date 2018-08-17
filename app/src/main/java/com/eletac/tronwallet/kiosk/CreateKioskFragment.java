d4ad6723998e83d491dccb70083a2518adb26c08
Marius Gill <35896407+Dryec@users.noreply.github.com>
2018-08-17 03:33:08 +0200

    create kiosk ui

---
+++ app/src/main/java/com/eletac/tronwallet/kiosk/CreateKioskFragment.java
@@ -0,0 +1,34 @@
+package com.eletac.tronwallet.kiosk;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.eletac.tronwallet.R;
+
+public class CreateKioskFragment extends Fragment {
+
+
+    public CreateKioskFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_kiosk, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
